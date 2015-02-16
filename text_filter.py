9c24ced41fc998b71518fe42ab17ebd0e3af99c6
ken a <aidken+github@gmail.com>
2015-02-16 23:49:28 +0900

    Create text_filter.py
    Text filter. Usually I want to do something to each line. Use regexp to extract part of line and build objects, etc.
    Let's find out how to use regexp, extract something and put it into a variable.
    Also, another basic thing. How can I receive argument from command line. Usually I want to receive file name from command line.

---
+++ text_filter.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+for line in open( "./first.py" ):  # don't forget colon at the end.
+  print( line, end='' )            # end='' suppresses newline at the end.
+                                   # each line has new line already.
+                                   # also indentation matters. be careful.
+                                   
