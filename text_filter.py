3ae66260023a8afeddaa36f2b094058021a74424
ken <aidken+github@gmail.com>
2015-02-19 00:22:39 +0900

    Receive argument from command line, added regexp
    first I tried this
    
    try:
      os.path.isfile( myfile )
    except:        # except: gets invoked upon exception
      print( "Cannot find file 'myfile'.", end="\n" )
      quit()
    
    only to find that False is not an exception in Python. False caused at try: is not caught by except:.

--- text_filter.py
+++ text_filter.py
@@ -1,7 +1,27 @@
 #! /usr/bin/env python3
 
-for line in open( "./first.py" ):  # don't forget colon at the end.
-  print( line, end='' )            # end='' suppresses newline at the end.
-                                   # each line has new line already.
-                                   # also indentation matters. be careful.
-                                   
+# a first python script
+import sys
+import os.path
+import re
+
+# receive argument from command line
+myfile = sys.argv[1]             # sys.argv is a list
+print( "myfile given is " + myfile, end='\n' )
+
+# sys.argv[0] is always the name of this very program
+
+# see if myfile really exists
+if not os.path.isfile( myfile ):
+  print( "Cannot find file 'myfile'.", end='\n' )
+  quit()                           # alternative sys.exit()
+
+for line in open( myfile ):        # don't forget colon at the end.
+  # let's suppress all comments
+  line = line.rstrip()             # remove newline
+  line = re.sub( '#.+', '', line ) # substitute anything after # with nothing
+  print( line, end='\n' )          # add newline by end='\n'
+                                   # if you don't want to add new line, use end=''
+
+
+
