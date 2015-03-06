b573ed7a7e4e58e0a2d7761b00c7541ad8e651ae
Ken Aida <aidken+github@gmail.com>
2015-03-06 21:46:47 +0900

    =Now using context manager.

--- text_filter.py
+++ text_filter.py
@@ -6,22 +6,19 @@ import os.path
 import re
 
 # receive argument from command line
-myfile = sys.argv[1]             # sys.argv is a list
-print( "myfile given is " + myfile, end='\n' )
+myfile = sys.argv[1]                 # sys.argv is a list
+print("myfile given is " + myfile)
 
 # sys.argv[0] is always the name of this very program
 
 # see if myfile really exists
 if not os.path.isfile( myfile ):
-  print( "Cannot find file 'myfile'.", end='\n' )
-  quit()                           # alternative sys.exit()
+  print("Cannot find file 'myfile'.")
+  quit()                             # alternative sys.exit()
 
-for line in open( myfile ):        # don't forget colon at the end.
-  # let's suppress all comments
-  line = line.rstrip()             # remove newline
-  line = re.sub( '#.+', '', line ) # substitute anything after # with nothing
-  print( line, end='\n' )          # add newline by end='\n'
-                                   # if you don't want to add new line, use end=''
-
-
-
+with open(myfile, encoding='cp932') as tmp: # context manager
+  for line in tmp:                   # don't forget colon at the end.
+    # let's suppress all comments
+    line = line.rstrip()             # remove newline
+    line = re.sub('#.+', '', line)   # substitute anything after # with nothing
+    print(line)
