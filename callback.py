b0288060b77e77d670b2c77558a6ffc8ab8dbbca
Ken Aida <aidken+github@gmail.com>
2015-03-19 00:19:43 +0900

    Update callback.py

--- callback.py
+++ callback.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 
+import types
+
 data = [(1,2), (2,3), (3,4)]
 
 def test(cb):
-  for x in data:
-    print(cb(x[0], x[1]))
+  if type(cb)==types.FunctionType:
+    for x in data:
+      print(cb(x[0], x[1]))
 
 def callback(a, b):
   return a+b
