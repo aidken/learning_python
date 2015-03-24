067e3bf4d52222cde3f1632a67f08d1f26d3c622
Ken Aida <aidken+github@gmail.com>
2015-03-24 18:41:45 +0900

    Update check_date.py

--- check_date.py
+++ check_date.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import datetime
-import re
 import logging
 
 logfile = './log.txt'
@@ -15,30 +14,30 @@ def check_date(tmp):
     Check a given value, and convert it to a datetime.date object if possible.
     Return None if not possible
     '''
-    if tmp is None or tmp == '':
-        logging.debug('returning None.')
-        return None
-    elif type(tmp).__name__ == datetime.date.__name__:
+    logging.debug('check_date received ' + tmp)
+
+    if type(tmp).__name__ == datetime.date.__name__:
         logging.debug('it\'s already a ' + type(tmp).__name__ + ' instance.')
         return tmp
+    elif tmp == None or tmp == '' or type(tmp).__name__ != type('x').__name__:
+        logging.debug('returning None.')
+        return None
     else:
         try:
-            x = re.search('^(\d\d\d\d).(\d\d).(\d\d)$', tmp)
-            return datetime.date(int(x.group(1)), int(x.group(2)), int(x.group(3)))
-        except:
+            x = datetime.datetime.strptime(tmp, "%Y-%m-%d")
+            return x
+        except ValueError:
             try:
-                x = re.search('^(\d\d).(\d\d).(\d\d\d\d)$', tmp)
-                return datetime.date(int(x.group(3)), int(x.group(1)), int(x.group(2)))
-            except:
+                x = datetime.datetime.strptime(tmp, "%m-%d-%Y")
+                return x
+            except ValueError:
                 try:
-                    x = re.search('^(\d\d).(\d\d).(\d\d)$', tmp)
-                    return datetime.date(2000+int(x.group(3)), int(x.group(1)), int(x.group(2)))
-                except:
+                    x = datetime.datetime.strptime(tmp, "%m-%d-%y")
+                    return x
+                except ValueError:
                     logging.debug('"' + tmp + '" met no conditions stated. Falling back to None.')
                     return None
 
-# tmp = check_date('02/14/15')
-tmp = datetime.date(2015,2,13)
-tmp = check_date(tmp)
-print(tmp.isoformat())
-
+tmp = check_date('02-14-15')
+# tmp = check_date('2015-02-15')
+print(tmp.strftime('%Y-%m-%d'))
