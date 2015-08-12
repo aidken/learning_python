23b41d6a7e0e3b842bfab9e1fa6b0731652a4052
Ken Aida <aidken+github@gmail.com>
2015-08-12 23:40:27 +0900

    Update regular_expression.py

--- regular_expression.py
+++ regular_expression.py
@@ -4,13 +4,18 @@ import re
 
 def remove_zeros(var):
     try:
-        m = re.match('^(\d+).00$', str(var))
+        m = re.match('^(\d+)\.00$', str(var))
         return m.group(1)
     # trap error in case match did not work
     except AttributeError:
         return var
 
-pieces = ['1.00',2.00,3]
+pieces = [
+    '1.00',
+    2.00,
+    3,
+    400
+]
 
 # list comprehension
 pieces = [remove_zeros(x) for x in pieces]
