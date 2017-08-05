2ae73280ef1b712b0683759dd550e14eb41ebfd4
aidken <aidken+github@gmail.com>
2017-08-05 17:00:16 +0900

    Update iostring.py

--- iostring.py
+++ iostring.py
@@ -19,7 +19,7 @@ def main():
         'Line #4'
         ]
 
-    with open('file.txt', 'w') as f:
+    with open('file1.txt', 'w') as f1:
 
         for x in tmp:
             myio.write(x)
@@ -28,10 +28,21 @@ def main():
         myio.seek(0)
 
         # cf. https://stackoverflow.com/questions/3253258/what-is-the-best-way-to-write-the-contents-of-a-stringio-to-a-file
-        shutil.copyfileobj(myio, f)
+
+        shutil.copyfileobj(myio, f1)
+        # ^- This puts contents of myio to file1.txt
+
+        myio.seek(0)
+
+        with open('file2.txt', 'w') as f2:
+
+            for x in myio:
+                print(x, file=f2)
+                # ^- This puts  contents of myio to file2.txt
 
     # What I want to do is to have an IO,
     # add data to it, and then create a file. Looks it works.
+    # Also, wanted to confirm if I treat an IO as a file.
 
 
 if __name__=='__main__':
