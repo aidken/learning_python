ea4d15a91ef8fe056648eb49f90eeaca14532bbf
Ken Aida <aidken+github@gmail.com>
2024-12-15 11:47:25 +0900

    Modified python_template_openpyxl.py. Now parse() method is part of the Report class.

--- python_template_openpyxl.py
+++ python_template_openpyxl.py
@@ -17,11 +17,43 @@ logger.addHandler(logging.NullHandler())
 
 @dataclass
 class Report:
-    file   : str
-    records: list = field(default_factory=list, init=False)
+    file_name     : str
+    worksheet_name: str = field(default='Sheet1')
+    row_of_label  : int = 1
+    records       : list = field(default_factory=list, init=False)
 
     def __post_init__(self):
         pass
+
+    def parse(self):
+        # iterate through rows of an Excel spreadsheet
+
+        wb = load_workbook(filename=self.file_name, read_only=True, data_only=True)
+        ws = wb[self.worksheet]
+        logger.debug(f'Slurp item information. Looking at worksheet "{ws.title}".')
+
+        logger.debug(f"Max row of this sheet is {ws.max_row}.")
+        for row_number, row in enumerate(
+            ws.iter_rows(min_row=self.row_of_label + 1, max_row=ws.max_row + 1, values_only=True), start=self.row_of_label + 1
+        ):
+            # In openpyxl, row and column are one based, not zero based. Cell "A1" is row 1, col 1.
+            # Note that when you iterate through rows, now a row is a tuple, and it is zero based.
+            # In this example, row[0] is column A.
+            # a note on cell value evaluation:
+            # the value of a blank cell is None.
+            # the value of zero length string (such as ="") is None
+            # if you set data_only True on your load_workbook().
+            # the value of zero length string (such as ="") is '=""'
+            # if you do not set data_only property True on your load_workbook().
+            self.records.append(
+                Record(
+                    row_number = row_number,
+                    some_str   = row[0],
+                    some_int   = row[1],
+                    some_float = row[2],
+                    some_date  = row[3],
+                )
+            )
 
 
 @dataclass
@@ -34,49 +66,6 @@ class Record:
 
     def __post_init__(self):
         pass
-
-
-def parse(file, worksheet, row_of_label, callback=None):
-
-    # iterate through rows of an Excel spreadsheet
-
-    if callback is not None and not callable(callback):
-        raise ValueError(
-            f'callback given but it it not callable. It is a {type(callback)}.'
-        )
-
-    wb = load_workbook(filename=file, read_only=True, data_only=True)
-    ws = wb[worksheet]
-    logger.debug(f'Slurp item information. Looking at worksheet "{ws.title}".')
-
-    r = Report(file=file)
-
-    logger.debug(f'Max row of this sheet is {ws.max_row}.')
-    for row_number, row in enumerate(ws.iter_rows(min_row=row_of_label+1, max_row=ws.max_row+1, values_only=True), start=row_of_label+1):
-        # In openpyxl, row and column are one based, not zero based. Cell "A1" is row 1, col 1.
-        # Note that when you iterate through rows, now a row is a tuple, and it is zero based.
-        # In this example, row[0] is column A.
-        # a note on cell value evaluation:
-        # the value of a blank cell is None.
-        # the value of zero length string (such as ="") is None
-        # if you set data_only True on your load_workbook().
-        # the value of zero length string (such as ="") is '=""'
-        # if you do not set data_only property True on your load_workbook().
-        r.records.append(
-            Record(
-                row_number = row_number,
-                some_str   = row[0],
-                some_int   = row[1],
-                some_float = row[2],
-                some_date  = row[3],
-            )
-        )
-
-        if callback is not None and callable(callback):
-            callback(r.records[-1])
-
-    if callback is None:
-        return r
 
 
 def main():
