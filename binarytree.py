da7d03453e4050c98a3e45d4e214d1bddad8df49
Ken Aida <aidken+github@gmail.com>
2025-08-10 17:54:51 +0900

    Adding binarytree.py.'

---
+++ binarytree.py
@@ -0,0 +1,19 @@
+# from Effective Python, 2nd Edition.
+
+# Item 43 Inherit from collections.abc for custom container types
+# https://effectivepython.com/
+
+class BinaryNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class IndextableNode(BinaryNode):
+    def _traverse(self):
+        if self.left is not None:
+            yield from self.left._traverse()
+        yield self
+        if self.right is not None:
+            yield from self.right._traverse()
